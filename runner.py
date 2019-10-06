b98d32f9441a4455e65920872b45528aaafef244
Jiayi Li <lijiayi9712@berkeley.edu>
2019-10-06 14:01:40 -0700

    step-by-tep assignment

--- runner.py
+++ runner.py
@@ -86,67 +86,130 @@ def init():
     ]
     traci.start(config)
 
-veh_states = {} #mapping each vehicle to a tuple (stop_counter, route)
+veh_states = {} #mapping each vehicle to a tuple (stop_counter, stop_set, route_set, mission)
 
 
 def step(time, missions, idle_vehs, working_vehs):
     random_num = np.random.uniform(0, 1)
-    if random_num <= 0.1 and len(idle_vehs) > 0:
+
+    if random_num <= 0.5 and len(idle_vehs) > 0:
         mission_index = np.random.choice(len(missions))
         mission = missions[mission_index]
         mission_veh = idle_vehs.pop()
         working_vehs.append(mission_veh)
-        route = choose_route(mission[0], mission[1])[:-1] + \
-                choose_route(mission[1], mission[2])[:-1] + \
-                choose_route(mission[2], mission[3])
+        # route = choose_route(mission[0], mission[1])[:-1] + \
+        #         choose_route(mission[1], mission[2])[:-1] + \
+        #         choose_route(mission[2], mission[3])
 
+        
+        # route_id = str(mission_veh) + \
+        #            mission[0] + '_' + \
+        #            mission[1] + '_' + \
+        #            mission[2] + '_' + \
+        #            mission[3] + '_' + \
+        #            str(time)
 
-        route_id = str(mission_veh) + \
-                   mission[0] + '_' + \
-                   mission[1] + '_' + \
-                   mission[2] + '_' + \
-                   mission[3] + '_' + \
-                   str(time)
+        veh_states[mission_veh] = (0, 0, 0, mission)
 
         traci.vehicle.setParkingAreaStop(
             vehID=mission_veh, 
-            stopID=mission[3], 
-            duration=0,
-            )
+            stopID=mission[0], 
+            duration=2,
+        )
 
-        traci.route.add(route_id, route)
-        traci.vehicle.setRoute(mission_veh, route)
-
-        duration_func = lambda x: 20
-        
-        for _ in range(30):
+        for t in range(1):
             traci.simulationStep()
-
-        traci.vehicle.setParkingAreaStop(
-            vehID=mission_veh, 
-            stopID=mission[1], 
-            duration=duration_func(mission[1]),
-        )
-
-        traci.vehicle.setParkingAreaStop(
-            vehID=mission_veh, 
-            stopID=mission[2], 
-            duration=duration_func(mission[2]),
-           
-        )
-        
-        traci.vehicle.setParkingAreaStop(
-                        vehID=mission_veh, 
-                        stopID='P4', 
-                        duration=1000000,
-                    )
-        
         finished = []
 
-        for veh in working_vehs:
-            if traci.vehicle.getSpeed(veh) == 0.0 and traci.vehicle.getRouteIndex(veh) == 'CB01':
-                finished.append(veh)
-                idle_vehs.append(veh)
+    for veh in working_vehs: 
+        counter = veh_states[veh][0]
+        stop_set = veh_states[veh][1]
+        route_set = veh_states[veh][2]
+        veh_stops = veh_states[veh][3]
+        
+        try: 
+            traci.vehicle.isStoppedParking(veh)
+ 
+        except traci.exceptions.TraCIException:
+            print(veh)
+            print(veh_stops)
+
+            print(counter)
+            print(stop_set)
+            print(route_set)
+
+        if traci.vehicle.isStoppedParking(veh) and stop_set == 0 and route_set == 0 and counter < 3:
+            earlier_stop = veh_stops[counter] 
+            new_stop = veh_stops[counter+1]
+            earlier_stop = veh_stops[counter]
+            new_stop = veh_stops[counter+1]
+
+            route_seg = choose_route(earlier_stop, new_stop)
+            route_id = str(veh) + \
+                   earlier_stop + '_' + \
+                   new_stop + '_' + \
+                   str(time)
+            traci.route.add(route_id, route_seg)
+            traci.vehicle.setRoute(veh, route_seg)
+            traci.simulationStep()
+            route_set = 1
+
+            duration_func = lambda x: 20 if x != 'P4' else 1000000
+            traci.vehicle.setParkingAreaStop(
+                vehID=veh, 
+                stopID=new_stop,
+                duration = duration_func(new_stop), 
+            )
+            stop_set = 1
+            counter += 1
+        # if stop_set == 0 and traci.vehicle.isStoppedParking(veh) == False and counter < 3:
+            
+        
+        if traci.vehicle.isStoppedParking(veh) == False:
+            stop_set = 0
+            route_set = 0
+        veh_states[veh] = (counter, stop_set, route_set, veh_stops)
+
+        
+        # if traci.vehicle.isStoppedParking(veh):
+        #     veh_states[veh] = (counter, stop_set, veh_stops)
+        # if traci.vehicle.isStoppedParking(veh) == False and stop_set == 0 and counter < 3: 
+        #     print("set stop 1")
+        #     earlier_stop = veh_stops[counter]
+        #     new_stop = veh_stops[counter+1]
+
+        #     route_seg = choose_route(earlier_stop, new_stop)
+
+
+        #     route_id = str(veh) + \
+        #            earlier_stop + '_' + \
+        #            new_stop + '_' + \
+        #            str(time)
+            
+        #     traci.route.add(route_id, route_seg)
+
+        #     traci.vehicle.setRoute(veh, route_seg)
+
+        #     print("set stop 2")
+        #     print(route_seg)
+        #     traci.simulationStep()
+            
+        #     duration_func = lambda x: 20 if x != 'P4' else 1000000
+
+            
+
+        #     traci.vehicle.setParkingAreaStop(
+        #         vehID=veh, 
+        #         stopID=new_stop,
+        #         duration = duration_func(new_stop), 
+        #     )
+        #     print("set stop 3")
+        #     veh_states[veh] = (counter+1, 1, 1, veh_stops)
+
+        if traci.vehicle.getSpeed(veh) == 0.0 and traci.vehicle.getRouteIndex(veh) == 'CB01':
+            finished.append(veh)
+            idle_vehs.append(veh)
+            veh_states[veh] = (0, 0, 0, veh_route)
         working_vehs = [veh for veh in working_vehs if veh not in idle_vehs]
     traci.simulationStep()
         
